a533663cf3196d40a8f60cd59fe433c58a10ab39
Sushant Shrestha <sstha813@gmail.com>
2026-03-23 01:58:45 +0000

    setting up server.js

---
+++ server/server.js
@@ -0,0 +1,10 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
