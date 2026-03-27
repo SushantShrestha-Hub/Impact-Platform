0f604c2af244464845d316495f4384dfc2e9af96
Sushant Shrestha <sstha813@gmail.com>
2026-03-27 00:38:56 +0000

    added other files

---
+++ server/config/testdb.js
@@ -0,0 +1,17 @@
+// import mongoose from "mongoose";
+
+// const MONGO_URI =
+//   "mongodb+srv://shresthasushant36_db_user:rrHjfGWJB9So9cNq@cluster0.b1l873g.mongodb.net/impactplatform?retryWrites=true&w=majority&appName=Cluster0";
+
+// console.log("Attempting connection...");
+
+// mongoose
+//   .connect(MONGO_URI)
+//   .then(() => {
+//     console.log("SUCCESS - MongoDB connected!");
+//     process.exit(0);
+//   })
+//   .catch((err) => {
+//     console.log("FAILED -", err.message);
+//     process.exit(1);
+//   });
